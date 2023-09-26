2d5438723605c0a8b7e90639fb9be0dbb16da103
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-09-26 23:50:54 +0700

    Инициализация проекта. Добавил README.md

---
+++ lab2/src/main/java/ru/nsu/bolotov/application/Application.java
@@ -0,0 +1,12 @@
+package ru.nsu.bolotov.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+    }
+}
